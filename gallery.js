96406470fe0d9967e44921eb7a44b1bf749b76fb
Jeremy Raines <jraines@gmail.com>
2015-07-24 13:47:59 -0400

    Extract OutfitView object, set up click listener on outfit views to render a bit outfit view as a modal on top of a mask, where clicking on the mask dismisses the modal and the mask'

--- gallery.js
+++ gallery.js
@@ -1,3 +1,7 @@
+//The Gallery object knows how to accept an array of JSON objects
+//representing outfits, and store a collection of our custom outfit
+//objects. It knows how to render a subset of those, and to page through
+//them
 function Gallery(api_outfits) {
   this.outfits = _.map(api_outfits, function(o){
     return new Outfit(o);
@@ -44,15 +48,54 @@ function Gallery(api_outfits) {
   }
 }
 
+
+//this object represents a View of an outfit
+//it knows how to render itself to the page,
+//and how to set up click listeners on itself
+function OutfitView(outfitModel){
+  var model = outfitModel;
+  var view = this;
+
+  this.div = function(){
+    $outfit = $('<div class="outfit"></div>');
+    $outfit.css('background-image', 'url(' + model.image_url + ')');
+    $outfit.hide();
+    return $outfit
+  }
+
+  this.setUpClick = function($div){
+    $div.on('click', function() {
+      imgUrl = model.image_url;
+
+      //TODO this could also be extracted into its own view object
+      $bigOutfitView = $('<div class="big-outfit"></div>');
+      $bigOutfitView.css('background-image', 'url(' + model.image_url + ')');
+
+      $mask = $('body').append('<div class="mask"></div>');
+      $('.mask').append($bigOutfitView);
+
+      $('.mask').on('click', function(){
+        $('.big-outfit').remove();
+        $('.mask').remove();
+      });
+
+    });
+  }
+
+  this.render = function(){
+    $outfitView = this.div();
+    $('#outfits').append($outfitView);
+    this.setUpClick($outfitView);
+    $outfitView.fadeIn();
+  }
+}
+
+//this is the outfit "model"
 function Outfit(data) {
   this.image_url = data.large_image_url;
 
   this.render = function(){
-    $outfitView = $('<div class="outfit"></div>');
-    $outfitView.css('background-image', 'url(' + this.image_url + ')');
-    $outfitView.hide();
-    $('#outfits').append($outfitView);
-    $outfitView.fadeIn();
+    new OutfitView(this).render();
   }
 }
 
